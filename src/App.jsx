78a9460deda695d24ac7315ad1a0feac2e3e9a88
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-03 22:45:45 +0530

    task7-react1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseItems from "./components/ExpenseItems";
+
+function App() {
+  return (
+    <>
+      <h1>shravani</h1>
+      <ExpenseItems></ExpenseItems>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.jsx
@@ -0,0 +1,11 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <h2>Expense items!</h2>
+      <div>june4</div>
+      <h2>car insurance</h2>
+      <div>$200</div>
+    </div>
+  );
+}
+export default ExpenseItems;
