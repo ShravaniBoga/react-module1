c0f6bba4793a5b4767a212c6c980c832a766bc98
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-04 19:54:36 +0530

    14th video of module1

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,9 +2,18 @@ import { Component } from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  console.log(props, "varun");
+  const month = props.item.date.toLocaleString("en-US", { month: "long" });
+  const day = props.item.date.toLocaleString("en-Us", { day: "2-digit" });
+  const year = props.item.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.item.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+
       <div className="expense-item__description">
         <h2>{props.item.title}</h2>
         <h2>{props.item.LocationOfExpenditure}</h2>
