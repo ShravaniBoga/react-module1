77474a2723fb15cfaf6c43dd9fb17460fe6cb7b0
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-04 21:47:59 +0530

    task10-module-1

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,24 +1,15 @@
-import { Component } from "react";
-import "./ExpenseItem.css";
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  console.log(props, "varun");
-  const month = props.item.date.toLocaleString("en-US", { month: "long" });
-  const day = props.item.date.toLocaleString("en-Us", { day: "2-digit" });
-  const year = props.item.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-
-      <div className="expense-item__description">
-        <h2>{props.item.title}</h2>
-        <h2>{props.item.LocationOfExpenditure}</h2>
-        <div className="expense-item__price">${props.item.amount}</div>
-      </div>
+      <ExpenseDate date={props.item.date}></ExpenseDate>
+      <ExpenseDetails
+        amount={props.item.amount}
+        title={props.item.title}
+        location={props.item.LocationOfExpenditure}
+      />
     </div>
   );
 }
