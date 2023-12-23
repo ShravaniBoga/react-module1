4ed7fb9306d1e26030062968ec6d69bdbc77583e
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-24 02:11:56 +0530

    module -2 task-9(filter updated)

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,40 @@
-import { useState } from "react";
-import ExpenseItem from "./components/ExpenseList/ExpenseItem";
-import NewExpense from "./components/NewExpense/NewExpense";
-const DUMMY_EXPENSES = [
-  // {
-  //   LocationOfExpenditure: "hyderabad",
-  //   title: "Toilet Paper",
-  //   amount: 94.12,
-  //   date: new Date(2020, 7, 14),
-  // },
-  // {
-  //   LocationOfExpenditure: "mumbai",
-  //   title: "New TV",
-  //   amount: 799.49,
-  //   date: new Date(2021, 2, 12),
-  // },
-  // {
-  //   LocationOfExpenditure: "Pune",
-  //   title: "Car Insurance",
-  //   amount: 294.67,
-  //   date: new Date(2021, 2, 28),
-  // },
-  // {
-  //   LocationOfExpenditure: "Kerla",
-  //   title: "New Desk (Wooden)",
-  //   amount: 450,
-  //   date: new Date(2021, 5, 12),
-  // },
-];
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   const addExpenseHandler = (expense) => {
-    console.log(expense, "app.js");
-    setExpenses((prevExpense) => {
-      return [...prevExpense, expense];
-    });
-  };
+    console.log('In App.js');
+    console.log(expense);
+
+  }
+
   return (
-    <>
-      <h1>let's start</h1>
+    <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      {/* <h2>ExpenseItems Details</h2> */}
-
-      {expenses.map((expense) => (
-
-        < ExpenseItem item={expense} />
-
-      ))
-      }
-    </>
+      <Expenses items={expenses} />
+    </div>
   );
-};
+}
 
 export default App;
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,9 @@
-import "./Card.css";
+import './Card.css';
 
-const Card = (props) => {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
+function Card(props) {
+    const classes = 'card ' + props.className;
+
+    return <div className={classes}>{props.children}</div>;
+}
+
 export default Card;
