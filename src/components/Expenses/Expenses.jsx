d134a2d9fc2b9eb02e1f1fc34cba5f9404377d04
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-24 09:03:19 +0530

    module2-task12(part1)

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -14,9 +14,34 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     })
-    let expensesContent = <p>No expenses found.</p>
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))
+    let expensesContent = ""
+    if (filteredExpenses.length === 0) {
+        expensesContent = <p>No expenses found.</p>
+    }
+    else if (filteredExpenses.length === 1) {
+        expensesContent = (
+            <div>
+                {filteredExpenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    ></ExpenseItem>
+                ))}
+                <p>Only single Expense here. Please add more...</p>
+            </div>
+        );
+    }
+    else if (filteredExpenses.length > 1) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            ></ExpenseItem>
+        ))
     }
     return (
         <Card className="expenses">
