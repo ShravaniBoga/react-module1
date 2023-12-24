30d10d45e0bb8bc7050cbaf65e22067c529ac2f0
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-24 07:40:24 +0530

    module-2 task-10(Rendering Expenses with performance optimisations(mapping list items dynamically))

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -14,7 +14,8 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            <ExpenseItem
+            {props.items.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
+            {/* <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
                 date={props.items[0].date}
@@ -33,7 +34,7 @@ const Expenses = (props) => {
                 title={props.items[3].title}
                 amount={props.items[3].amount}
                 date={props.items[3].date}
-            />
+            /> */}
         </Card>
     );
 }
