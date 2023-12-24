d53da6141f52b1694be16fe57f25a635c1081e56
BOGA SHRAVANI <shravaniboga07@gmail.com>
2023-12-24 07:54:32 +0530

    module-2 task-11(filtering-listitems-by year)

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -11,10 +11,13 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+    const filterdExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {props.items.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
+            {filterdExpenses.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
             {/* <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
